2fea7980340ecb6fe16fdfb484a6a50b333592d2
Compu Care <Compu Care@DESKTOP-DN4BUBJ>
2025-07-06 05:38:39 +0300

    Add project files.

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Estimate for carpet cleaning service");
+            Console.WriteLine($" Number of small carpets : ");
+            int smallcarpets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($" Number of large carpets : ");
+            int largecarpets = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" price per small room :$ 25 ");
+            Console.WriteLine(" price per large room :$ 35 ");
+
+            double cost = smallcarpets * 25 + largecarpets * 35;
+            double tax = cost * 0.06;
+            double total = cost * tax;
+            Console.WriteLine($" cost : {cost} ");
+            Console.WriteLine($" tax : {tax} ");
+            Console.WriteLine($" total : {total} ");
+
+            Console.WriteLine(" this estimate is valid for30 days ");
+        }
+    }
+}
